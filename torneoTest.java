0d859a373bd1a81c9ebb61b86db752d8deff881c
SimonPero <simonraulpero@gmail.com>
2026-06-04 22:22:46 -0300

    first commit

---
+++ torneoTest.java
@@ -0,0 +1,40 @@
+public class torneoTest {
+
+    public static void funcion1(){
+
+    }
+    public static void funcion2(){
+        
+    }
+    public static void funcion3(){
+        
+    }
+    public static void funcion4(){
+        
+    }
+
+
+    public static void funcion5(){
+        
+    }
+
+    public static void funcion6(){
+        
+    }
+
+    public static void funcion7(){
+        
+    }
+    public static void funcion8(){
+        
+    }
+    public static void funcion9(){
+        
+    }
+    public static void funcion10(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
