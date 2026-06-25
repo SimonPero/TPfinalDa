2c056157d184638646ff95d2224cd15595f071ec
SimonPero <simonraulpero@gmail.com>
2026-06-25 08:49:37 -0300

    cambios de souts dejados en el camino

--- Utils/UtilidadesTorneo.java
+++ Utils/UtilidadesTorneo.java
@@ -194,7 +194,6 @@ public class UtilidadesTorneo {
                     char num = codigo.charAt(i);
 
                     if (!Character.isDigit(num)) {
-                        System.out.println("asdas");
                         flag = false;
                     }
                     i++;
@@ -419,6 +418,7 @@ public class UtilidadesTorneo {
             escritor.newLine();
             escritor.write("=================================================================");
             if (arr.length > 0 && arr[0] != null) {
+                escritor.newLine();
                 escritor.write("Dia del Torneo: " + arr[0].getDia());
                 escritor.newLine();
                 escritor.write("-----------------------------------------------------------------");
